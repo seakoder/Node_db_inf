a08690e9d458ac62d7a91e4912481c78a6eab57d
seakoder <alloffi@protonmail.com>
2023-05-13 16:52:44 +0530

    updated the package files

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const defaultRouter = require('./Routes/defaultRouter')
 const productRouter = require('./Routes/productRouter');
 const a = x();
 
-a.listen(3000, ()=> console.log('this is a'));
-a.use(x.json());
+const PORT = process.env.PORT || 3000;
 
+a.listen(PORT, ()=> console.log('this is dummy text'));
+a.use(x.json());
 a.use('/' , defaultRouter);
 a.use('/api/products', productRouter);
-
 a.get('/health', health);
 a.get('*', notFound);
